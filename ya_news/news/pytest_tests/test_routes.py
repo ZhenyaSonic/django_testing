e5d16345968f215709cb9c2486089f6447a737cf
Евгений Братанов <sonik.zhenyaa@gmail.com>
2023-11-11 14:40:08 +0300

    2

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -27,7 +27,6 @@ def test_availability_for_comment_edit_and_delete(author_client,
                                                   client,
                                                   news_list
                                                   ):
-
     user_statuses = [
         (author_client, HTTPStatus.OK),
         (client, HTTPStatus.NOT_FOUND),
